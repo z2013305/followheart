4c39ddcace28a0bf7897a3d5e732eb50693e57c9
z2013305 <1350388707@qq.com>
2020-01-04 13:58:21 +0800

    提交代码以及基于electron-vue-admin，去除登陆以及table需要从服务器获取list

---
+++ src/renderer/views/function/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app-container">
+      <div>报表</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'function',
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
+
